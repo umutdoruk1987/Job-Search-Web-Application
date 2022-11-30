051d87fcc96469c07df0f8b263086b0692312cc8
Umut Doruk <umutdoruk1987@gmail.com>
2022-11-30 15:32:48 +0100

    add fields and annotations of employer class

--- src/main/java/com/umutdoruk/hrms/entities/concretes/Employer.java
+++ src/main/java/com/umutdoruk/hrms/entities/concretes/Employer.java
@@ -1,4 +1,37 @@
 package com.umutdoruk.hrms.entities.concretes;
 
-public class Employer {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@PrimaryKeyJoinColumn(name = "user_id")
+@Table(name = "employers")
+public class Employer extends User{
+
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "website")
+    private String website;
+
+    @Column(name = "telephone_number")
+    private String telephoneNumber;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @OneToMany(mappedBy = "employer")
+    private List<JobAdvertisement> jobAdvertisements;
+
 }
