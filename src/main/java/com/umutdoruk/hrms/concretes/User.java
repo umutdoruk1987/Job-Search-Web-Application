07ee5dabc6e0719117bd7a53a6f5777f32f8ad2d
Umut Doruk <umutdoruk1987@gmail.com>
2022-11-30 13:23:29 +0100

    add fields and annotations of user class

---
+++ src/main/java/com/umutdoruk/hrms/concretes/User.java
@@ -0,0 +1,36 @@
+package com.umutdoruk.hrms.concretes;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "confirm_password")
+    private String confirmPassword;
+
+    @Column(name = "create_date")
+    private Date createdDate;
+
+    @Column(name = "active")
+    private boolean isActive;
+}
--- src/main/java/com/umutdoruk/hrms/entities/concretes/Technology.java
+++
@@ -1,4 +0,0 @@
-package com.umutdoruk.hrms.entities.concretes;
-
-public class Technology {
-}
