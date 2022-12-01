25a509a8bf76b76c23b287c72b55d50d11ea4eb4
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-01 18:49:30 +0100

    add fields and annotations of TypeOfWork class

--- src/main/java/com/umutdoruk/hrms/entities/concretes/TypeOfWork.java
+++ src/main/java/com/umutdoruk/hrms/entities/concretes/TypeOfWork.java
@@ -1,4 +1,35 @@
 package com.umutdoruk.hrms.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "type_of_works")
 public class TypeOfWork {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "type_of_work_id")
+    private int typeOfWorkId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @OneToMany(mappedBy = "typeOfWork")
+    private List<JobAdvertisement> jobAdvertisements;
+
 }
