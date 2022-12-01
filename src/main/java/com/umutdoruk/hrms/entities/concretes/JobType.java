af21d204e0bdbfd02d1bd9a207b23577d5e28317
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-01 18:41:43 +0100

    add fields and annotations of JobType class

--- src/main/java/com/umutdoruk/hrms/entities/concretes/JobType.java
+++ src/main/java/com/umutdoruk/hrms/entities/concretes/JobType.java
@@ -1,4 +1,37 @@
 package com.umutdoruk.hrms.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "job_type")
 public class JobType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_type_id")
+    private int jobTypeId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @OneToMany(mappedBy = "jobType")
+    private List<JobAdvertisement> jobAdvertisements;
+
+
 }
