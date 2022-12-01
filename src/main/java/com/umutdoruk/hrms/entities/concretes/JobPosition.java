0b51587ef3167b81625c818f64a8fdb863a171d3
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-01 18:31:57 +0100

    add fields and annotations of JobPosition class

--- src/main/java/com/umutdoruk/hrms/entities/concretes/JobPosition.java
+++ src/main/java/com/umutdoruk/hrms/entities/concretes/JobPosition.java
@@ -1,4 +1,39 @@
 package com.umutdoruk.hrms.entities.concretes;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "job_positions")
 public class JobPosition {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_position_id")
+    private int jobPositionId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @OneToMany(mappedBy = "jobPosition")
+    private List<JobAdvertisement> jobAdvertisements;
+
+
+
 }
