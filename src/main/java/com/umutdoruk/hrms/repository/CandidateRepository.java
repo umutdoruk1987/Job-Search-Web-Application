c073b3a8bc52a9e35784ddc05fc43cf119d7e60e
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-06 23:44:46 +0100

    add methods of CandidateRepository class

--- src/main/java/com/umutdoruk/hrms/repository/CandidateRepository.java
+++ src/main/java/com/umutdoruk/hrms/repository/CandidateRepository.java
@@ -1,4 +1,13 @@
 package com.umutdoruk.hrms.repository;
 
-public interface CandidateRepository {
+import com.umutdoruk.hrms.entities.concretes.Candidate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CandidateRepository extends JpaRepository<Candidate, Integer> {
+
+    List<Candidate> findByEmail(String email);
+
 }
