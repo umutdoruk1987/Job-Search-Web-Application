5304bb2587bf8a5e00b047a0fb8c2032d04f6500
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-06 23:58:54 +0100

    add APIs of CandidatesController class

--- src/main/java/com/umutdoruk/hrms/controllers/CandidatesController.java
+++ src/main/java/com/umutdoruk/hrms/controllers/CandidatesController.java
@@ -1,4 +1,41 @@
 package com.umutdoruk.hrms.controllers;
 
+import com.umutdoruk.hrms.entities.concretes.Candidate;
+import com.umutdoruk.hrms.service.abstracts.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/candidates")
 public class CandidatesController {
+
+    private final CandidateService candidateService;
+
+    @Autowired
+    public CandidatesController (CandidateService candidateService){
+        this.candidateService=candidateService;
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Candidate>> getAll(){
+        return ResponseEntity.ok(this.candidateService.getAll());
+    }
+
+    @GetMapping("/getByEmail")
+    public ResponseEntity<List<Candidate>>  getByEmail(String email) {
+        return ResponseEntity.ok(this.candidateService.findByEmployerEmail(email));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@RequestBody Candidate candidate){
+        this.candidateService.add(candidate);
+        return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+
+
+
 }
