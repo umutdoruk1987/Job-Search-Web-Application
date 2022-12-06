78cb0fe528f615aa3cb4c2ecd4343d8585778670
Umut Doruk <umutdoruk1987@gmail.com>
2022-12-06 23:36:15 +0100

    add fields of CandidateService

--- src/main/java/com/umutdoruk/hrms/service/abstracts/CandidateService.java
+++ src/main/java/com/umutdoruk/hrms/service/abstracts/CandidateService.java
@@ -1,4 +1,12 @@
 package com.umutdoruk.hrms.service.abstracts;
 
+import com.umutdoruk.hrms.entities.concretes.Candidate;
+import java.util.List;
+
 public interface CandidateService {
+
+    void add(Candidate candidate);
+    List<Candidate> getAll();
+    List<Candidate> findByEmployerEmail(String email);
+
 }
