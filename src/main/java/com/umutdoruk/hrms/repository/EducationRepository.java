4006762b7d2d099eb120e612545fde93ca34dfcd
Umut Doruk <umutdoruk1987@gmail.com>
2023-02-01 12:09:44 +0100

    Add a @Query to some repositories that get all Data with resumeId

--- src/main/java/com/umutdoruk/hrms/repository/EducationRepository.java
+++ src/main/java/com/umutdoruk/hrms/repository/EducationRepository.java
@@ -2,10 +2,15 @@ package com.umutdoruk.hrms.repository;
 
 import com.umutdoruk.hrms.entities.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface EducationRepository extends JpaRepository<Education,Long> {
     List<Education> findAllByOrderByGraduationDateAsc();
     List<Education> findAllByOrderByGraduationDateDesc();
+
+    @Query(value = "SELECT * FROM educations e where e.resume_id = :resumeId", nativeQuery = true)
+    List<Education> findAllByResumeId(@Param("resumeId")Long resumeId);
 }
--- src/main/java/com/umutdoruk/hrms/repository/WorkExperienceRepository.java
+++ src/main/java/com/umutdoruk/hrms/repository/WorkExperienceRepository.java
@@ -2,6 +2,8 @@ package com.umutdoruk.hrms.repository;
 
 import com.umutdoruk.hrms.entities.WorkExperience;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,7 @@ import java.util.List;
 @Repository
 public interface WorkExperienceRepository extends JpaRepository<WorkExperience,Long> {
     List<WorkExperience> findAllByOrderByEndDateDesc();
+
+    @Query(value = "SELECT * FROM work_experiences we where we.resume_id = :resumeId", nativeQuery = true)
+    List<WorkExperience> findAllByResumeId(@Param("resumeId")Long resumeId);
 }
