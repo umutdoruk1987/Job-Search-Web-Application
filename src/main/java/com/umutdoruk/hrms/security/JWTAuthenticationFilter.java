3a57a70d5e103197a69e6725ab5e632d3da3b6ef
Umut Doruk <umutdoruk1987@gmail.com>
2023-02-06 23:03:57 +0100

    Add SecurityConfig, JWTAuthenticationFilter, JwtUtils and PasswordEncoder classes to project

---
+++ src/main/java/com/umutdoruk/hrms/security/JWTAuthenticationFilter.java
@@ -0,0 +1,69 @@
+package com.umutdoruk.hrms.security;
+
+import com.umutdoruk.hrms.exception.UnauthorizedException;
+import com.umutdoruk.hrms.security.utils.JwtUtils;
+import com.umutdoruk.hrms.service.serviceImpls.UserServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+/*import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;*/
+import java.io.IOException;
+import java.util.ArrayList;
+
+@RequiredArgsConstructor
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        try{
+            String jwtToken=jwtParser(request);
+            if(jwtToken!=null && jwtUtils.isJWTTokenValid(jwtToken)) {
+                String username=jwtUtils.getUserFromJWTToken(jwtToken);
+                UserDetails userDetails= userService.loadUserByUsername(username);
+
+                UsernamePasswordAuthenticationToken authentication =
+                     UsernamePasswordAuthenticationToken.authenticated(userDetails, null,userDetails.getAuthorities());   /*new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());*/
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        }catch (Exception e){
+            throw new UnauthorizedException(e.getMessage());
+
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String jwtParser(HttpServletRequest request ){
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith("Bearer ")){
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/umutdoruk/hrms/security/utils/JwtUtils.java
@@ -0,0 +1,51 @@
+package com.umutdoruk.hrms.security.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+
+import com.umutdoruk.hrms.entities.User;
+import com.umutdoruk.hrms.exception.UnauthorizedException;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private final String jwtSecret= "JobSearchAPI";
+
+    public String generateToken (User user){
+
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes());
+
+        return JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 100 * 60 * 1000))
+                .withIssuer("Umut Doruk")
+                .sign(algorithm);
+    }
+
+    public String getUserFromJWTToken(String jwtToken){
+
+        return JWT.decode(jwtToken).getSubject();
+
+    }
+
+    public Boolean isJWTTokenValid(String jwtToken){
+
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret.getBytes());
+        JWTVerifier verifier= JWT.require(algorithm).build();
+        try {
+            /*DecodedJWT decodedJWT= */
+            verifier.verify(jwtToken);
+            return true;
+        }catch (Exception e){
+            throw new UnauthorizedException(e.getMessage());
+        }
+
+    }
+
+}
